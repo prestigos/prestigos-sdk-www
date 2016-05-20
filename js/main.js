1813cb2c40d07d99c7a49579bb278a77c82056ab
Christopher Luna <dev@christopher-luna.com>
2016-05-20 04:21:45 -0500

    Fixed lint

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 /* jslint node: true */
 /*global window */
 'use strict';
-var $ = require('jquery');
+var $ = require('jquery'),
+  navHeight;
 require('modernizr');
 require('bootstrap');
 require('../bower_components/prism/prism.js');
@@ -12,29 +13,29 @@ $('#sidebar').affix({
   }
 });
 
-var navHeight = $('#header').outerHeight(true) + 180;
+navHeight = $('#header').outerHeight(true) + 180;
 
 $('body').scrollspy({
-  target: '#leftCol',
-  offset: navHeight
+  target : '#leftCol',
+  offset : navHeight
 });
 
-$('a').click(function() {
-  var href   = $.attr(this, 'href'),
-      anchor = $($.attr(this, 'href'));
+$('a').click(function () {
+  var href = $.attr(this, 'href'),
+    anchor = $($.attr(this, 'href'));
 
   // Do animation if the anchor exists
-  if(anchor.length > 0) {
+  if (anchor.length > 0) {
     $('html, body').animate({
       scrollTop: Number(anchor.offset().top)
-    }, 500,  function () {
+    }, 500, function () {
       window.location.hash = href;
     });
     return false;
   }
 
   // Do the default action if not
-  if(anchor.length === 0) {
+  if (anchor.length === 0) {
     return true;
   }
 });
